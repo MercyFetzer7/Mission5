8855547761d17d8821ec63ebc58c56384cf483e8
Mercy Fetzer <131705906+MercyFetzer7@users.noreply.github.com>
2025-02-03 14:13:44 -0700

    2:13 pm

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function(){
+    // When user changes the number of hours, hide the grand total
+    $('#numHours').on('input', function() {
+        $('#grandTotal').hide();
+    });
+
+    // Calculate total when the button is clicked
+    $('#btnCalculate').click(function(){
+        var hours = $('#numHours').val();
+        if (hours <= 0 || isNaN(hours)) {
+            alert('Please enter a valid positive number for hours.');
+        } else {
+            var chargePerHour = 50;
+            var total = chargePerHour * hours;
+            $('#grandTotal').text('Grand Total: $' + total).show();
+        }
+    });
+});
